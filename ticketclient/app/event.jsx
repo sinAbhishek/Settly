f40d9a7b6b1fbb3a855adb1c9a8bd6d54d091dcd
sinAbhishek <singhabhshek979@gmail.com>
2023-09-13 22:01:17 +0530

    final

--- ticketclient/app/event.jsx
+++ ticketclient/app/event.jsx
@@ -6,11 +6,13 @@ const Event = ({ event }) => {
   //   console.log(event);
   const { user } = useContext(AuthContext);
   return (
-    <div>
+    <div className=" my-2">
       <img className="w-screen h-80 bg-cover" src={event.image} alt="" />
 
-      <div className=" w-screen h-12 bg-gray-900 flex items-center justify-between">
-        <h1 className="text-slate-100 font-bold text-lg">{event.eventtitle}</h1>
+      <div className=" w-screen h-max bg-gray-900 flex items-center justify-between py-8 pr-4">
+        <h1 className="text-slate-100 font-bold text-lg ml-2">
+          {event.eventtitle.substring(0, 70)}...
+        </h1>
         <Link
           href={{
             pathname: user ? `/${event.eventtitle}` : "/login",
@@ -19,7 +21,7 @@ const Event = ({ event }) => {
             },
           }}
         >
-          <button className=" bg-pink-600 text-white font-medium px-4 py-1 rounded-md">
+          <button className=" bg-pink-600 w-36 text-white font-medium px-4 py-1 rounded-md mr-2">
             Get Details
           </button>
         </Link>
